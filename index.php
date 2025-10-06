166b1aba1fdc166da28c6bd1c1d1cd3e9b97b4ac
Dante <dante.160110@gmail.com>
2025-10-06 17:12:44 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>conecta JOVEM</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </head>
+    <style>
+    .verde {
+        background-color:#2F4F4F;
+        color: white
+        
+    }
+    .bandeira {
+        text-align: center
+    }
+    h2 { font-family: Arial Black, sans-serif; margin-left: 20px;}    
+    </style>
+    <body>
+    <div class="verde row justify-content row-cols-1 row-cols-md-3 mb-3 text">
+        <h2>SEJUC</h2>
+        <img src="https://mail-attachment.googleusercontent.com/attachment/u/0/?ui=2&ik=8044bc6ddb&attid=0.1&permmsgid=msg-f:1845263873557491827&th=199bb1b764417073&view=att&disp=inline&realattid=f_mgfk0l420&zw&saddbat=ANGjdJ8lYUuvnG9tO0Z4PwWM4W0uQNF-ccrVxE2CHrhNdjcD58lCpmEAFA9TLslaVOwQpepKNR7OyM0-2r_CKvbi7Wmvs0cZXuU4IWjcoj0ZwpCsBm_ro8nh0O3wwA1McvMnko3KwGEcBbtgXMp3x7tjbwEkivZ47O6MaCy1LQiumIbw2HSqbdPUY6ukJwtVqp5GK75kE3bhFRdTqfTGmG5uGsc2uJMryT2W1g7TucAEZXIkzWyEpMsHKFUMH6AHUa21iIHcPnYKnGUdL2cKBta4DOaYehx48b49o8rS6MD86u9ivmt_zR6MtBXwJkJEWWh6K-tkICB0QcwTV-isfikdq4tkcjHBGMUR0PDjwoJNCZ01lkzLz8Ia7ndJcHM7Iefts_rwaRclAibtDnLURaLOwc21jquburQycPY-t-cF5g0ExQG4nsQR47j9frSpKye6FJ05u5CTox86b_KlKHxv6iVS4CRay8fFmz7B3RXPy8zxrgv8g-wAogxT3vDo4SGtJv5DG-q1PXSbBoMkXCH8iJ1Lvr4uXxGdfJtdJOSlXptwn2Qzo5V4KDjra8-MdMK4Tt29AolblsJb_uZCRHFVVo_5d-xcnwQYhtDA5hwnh32wT6SVM-35shPYNDG2-7ijYrhxVuGVRpz1oSTK5bXK-ylE9UR-4bS85-z9ClxSsWDmOQkFpZi094a75keBEs8uc3nWH1VzVv0DQaIWEJCvKIIGLu5I9731h-6WCrpsDKlVcjYD9ynR8qTWBDcASpbD8CSfcc-TNBK3MkUBMz74EH4JVmOWFlaNrMFToacocZ5cWDw6cWvp6_GdCgjRMJogVp7hUnuRO3QD3wQLdgaFXNWSFLnvirEhu7SRY45IAr6Rz9P2UkooMFoDzoFWzwZzcBWyTt599CewmxLF4GQZI7MsCJDbAebwGIrgj_GZ9dgWJf8_XQ4g3kwSGYW9vKE6oukwtpR10aaIhWXifkvmWFH7KgTdGpijGYhzXTRi-zHUaafPe7IXgkTPC0Uzg3WNGtnSAuzhXxfBEf30T79ZoG3KY1_w3ZohBru5MA" style = " margin-left: 20px; width:150px; height:50px"/>
+    </div>    
+    <div class="bandeira style=text-align: center;">
+    <h2>
+        CONECTA JOVEM
+    </h2>
+    <img src="https://mail.google.com/mail/u/0/?ui=2&ik=8044bc6ddb&attid=0.1&permmsgid=msg-f:1845260140354816416&th=199bae52300949a0&view=fimg&disp=thd&attbid=ANGjdJ_uaCr9ULhdhmEfnhFthM9opeDqqwTwi5shEawtxwSWR9rYPKkUjGjaE0mNHZ1R7yNB7nBxhYpr07IVuS36MDDZrtBHnt70bUw8hvaydOU9XHRtVGF5ScttujE&ats=2524608000000&sz=w1920-h953" alt="" style = "position relative;bottom: 30px; marin-bottom: 60px;float: center; width:150px; height:150px"/>   
+    </div>
+        <br>
+        <br>
+        <div class=" row justify-content-center row-cols-1 row-cols-md-3 mb-3 text-center">
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sw">
+                    <div class="card-header py-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#00008B" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                        </svg>&nbsp;&nbsp;<b>LOGIN</b></svg>
+                    </div>
+                    <div class="card-body text-start">
+                        <form action="login.php" method="POST">
+                            <label class="form-label"><b>LOGIN</b></label>
+                            <input class="form-control" type="text" name="login" required/>
+                            <br>
+                            <label class="form-label"><b>SENHA</b></label>
+                            <input class="form-control" type="password" name="senha" required/>
+                            <br>
+                            <input type="submit" class="btn btn-outline-success" value="LOGIN">
+                        </form>
+                    </div>
+                </div>       
+            </div>
+        </div>
+    <body>
+</html>
