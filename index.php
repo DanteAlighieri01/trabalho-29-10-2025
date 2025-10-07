837a09593c2d9ac4ea7bf6c4a9589a7808d48997
Dante <dante.160110@gmail.com>
2025-10-07 14:46:29 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <html lang="pt-br">
     <head>
         <meta charset="utf-8">
-        <title>conecta JOVEM</title>
+        <title>SEJUC/CONECTA JOVEM</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </head>
     <style>
     .verde {
-        background-color:#2F4F4F;
+        background-color:#696969;
         color: white
         
     }
@@ -19,7 +19,6 @@
     </style>
     <body>
     <div class="verde row justify-content row-cols-1 row-cols-md-3 mb-3 text">
-        <h2>SEJUC</h2>
         <img src="https://mail-attachment.googleusercontent.com/attachment/u/0/?ui=2&ik=8044bc6ddb&attid=0.1&permmsgid=msg-f:1845263873557491827&th=199bb1b764417073&view=att&disp=inline&realattid=f_mgfk0l420&zw&saddbat=ANGjdJ8lYUuvnG9tO0Z4PwWM4W0uQNF-ccrVxE2CHrhNdjcD58lCpmEAFA9TLslaVOwQpepKNR7OyM0-2r_CKvbi7Wmvs0cZXuU4IWjcoj0ZwpCsBm_ro8nh0O3wwA1McvMnko3KwGEcBbtgXMp3x7tjbwEkivZ47O6MaCy1LQiumIbw2HSqbdPUY6ukJwtVqp5GK75kE3bhFRdTqfTGmG5uGsc2uJMryT2W1g7TucAEZXIkzWyEpMsHKFUMH6AHUa21iIHcPnYKnGUdL2cKBta4DOaYehx48b49o8rS6MD86u9ivmt_zR6MtBXwJkJEWWh6K-tkICB0QcwTV-isfikdq4tkcjHBGMUR0PDjwoJNCZ01lkzLz8Ia7ndJcHM7Iefts_rwaRclAibtDnLURaLOwc21jquburQycPY-t-cF5g0ExQG4nsQR47j9frSpKye6FJ05u5CTox86b_KlKHxv6iVS4CRay8fFmz7B3RXPy8zxrgv8g-wAogxT3vDo4SGtJv5DG-q1PXSbBoMkXCH8iJ1Lvr4uXxGdfJtdJOSlXptwn2Qzo5V4KDjra8-MdMK4Tt29AolblsJb_uZCRHFVVo_5d-xcnwQYhtDA5hwnh32wT6SVM-35shPYNDG2-7ijYrhxVuGVRpz1oSTK5bXK-ylE9UR-4bS85-z9ClxSsWDmOQkFpZi094a75keBEs8uc3nWH1VzVv0DQaIWEJCvKIIGLu5I9731h-6WCrpsDKlVcjYD9ynR8qTWBDcASpbD8CSfcc-TNBK3MkUBMz74EH4JVmOWFlaNrMFToacocZ5cWDw6cWvp6_GdCgjRMJogVp7hUnuRO3QD3wQLdgaFXNWSFLnvirEhu7SRY45IAr6Rz9P2UkooMFoDzoFWzwZzcBWyTt599CewmxLF4GQZI7MsCJDbAebwGIrgj_GZ9dgWJf8_XQ4g3kwSGYW9vKE6oukwtpR10aaIhWXifkvmWFH7KgTdGpijGYhzXTRi-zHUaafPe7IXgkTPC0Uzg3WNGtnSAuzhXxfBEf30T79ZoG3KY1_w3ZohBru5MA" style = " margin-left: 20px; width:150px; height:50px"/>
     </div>    
     <div class="bandeira style=text-align: center;">
@@ -32,7 +31,7 @@
         <br>
         <div class=" row justify-content-center row-cols-1 row-cols-md-3 mb-3 text-center">
             <div class="col">
-                <div class="card mb-4 rounded-3 shadow-sw">
+                <div class="cinza card mb-4 rounded-3 shadow-sw">
                     <div class="card-header py-3">
                         <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#00008B" class="bi bi-person-circle" viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
